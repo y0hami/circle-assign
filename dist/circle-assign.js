24fe1e64fbfc353b134312cb03b00ba326c7a937
Sean Hamilton <sean@hamistudios.com>
2018-11-28 20:41:35 +0000

    chore(release): 2.0.0 build

--- dist/circle-assign.js
+++ dist/circle-assign.js
@@ -1,100 +1,127 @@
-"use strict";
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.circleAssign = factory());
+}(this, (function () { 'use strict';
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-/**
- * Deep object assign
- *
- * @param {Object} target The target object object
- * @param {...Object} sources The source object(s)
- */
-function circleAssign(target) {
-  if (_typeof(target) !== 'object' || target === undefined || target === null) {
-    target = {};
+  /**
+   * Check if a value is an object
+   *
+   * @param {*} o The value to check
+   *
+   * @returns {boolean} Whether or not it is an object
+   */
+  function isObj(o) {
+    return o instanceof Object && o.constructor === Object;
   }
+  /**
+   * Merge the specified source object into the target object
+   *
+   * @param {Object} target The base target object
+   * @param {Object} source The object to merge into the target
+   *
+   * @returns {Object} The merged object
+   */
 
-  var assign = function assign(target, source) {
-    var // get an array of keys from both target and source
-    targetKeys = Object.keys(target),
-        sourceKeys = Object.keys(source),
-        // create an empty object for the result
-    result = {}; // go through all the target keys
 
-    for (var i = 0; i < targetKeys.length; i++) {
-      var // get the current key
-      key = targetKeys[i]; // if the source keys contains the current key from target
+  function mergeObject(target, source) {
+    // create a variable to hold the target object
+    // so it can be changed if its not an object
+    var targetObject = target;
+    var sourceObject = source;
 
+    if (!isObj(target)) {
+      targetObject = {};
+    }
+
+    if (!isObj(source)) {
+      sourceObject = {};
+    } // get the object keys for the target and source objects
+
+
+    var targetKeys = Object.keys(targetObject);
+    var sourceKeys = Object.keys(sourceObject); // create a empty object for the result
+
+    var result = {}; // go through all the target keys
+
+    targetKeys.forEach(function (key) {
+      // check if the source object contains the key
       if (sourceKeys.indexOf(key) !== -1) {
-        // check if the target is null if it is
-        // set the result as the source, this
-        // should be fine since if the source is
-        // null it isn't overriding and if it
-        // isn't null it is overriding
-        if (target[key] === null) {
-          result[key] = source[key];
-        } else if (_typeof(target[key]) === 'object' && !(target[key] instanceof Array)) {
-          // check if the current target object value is an object
-          // check if the corresponding source value is also an object
-          if (_typeof(source[key]) === 'object' && !(source[key] instanceof Array)) {
-            // if both target and source values are objects rerun
-            // the assign function with the target and source being
-            // the current values
-            result[key] = assign(target[key], source[key]);
+        // check if the target value is null if it is
+        // set the result as the source value, this
+        // should be fine because if the source value
+        // is null it isn't overriding the target value
+        // and if it isn't null it is overriding
+        // as expected
+        if (targetObject[key] === null) {
+          result[key] = sourceObject[key];
+        } else if (isObj(targetObject[key])) {
+          // check if the source value is an object if
+          // it is then we need to merge both objects and
+          // set the result value to the merged object
+          if (isObj(sourceObject[key])) {
+            result[key] = mergeObject(targetObject[key], sourceObject[key]);
           } else {
-            // if the source value isn't an object assign the result key
-            // as the source value directly (this will override target objects
-            // with the new value from source)
-            result[key] = source[key];
+            // if the source value isn't an object we can
+            // simply override the value
+            result[key] = sourceObject[key];
           }
         } else {
-          // if the current target value isn't an object assign the result
-          // key as the source key (this will set strings, numbers, booleans etc.)
-          result[key] = source[key];
+          // if the target value isn't an object we can
+          // simply override the value
+          result[key] = sourceObject[key];
         }
       } else {
-        // if the source doesn't have the target key set the result to the
-        // old value (this will happen for keys which aren't defined in the source)
-        result[key] = target[key];
+        // if the source doesn't contain the key set the result
+        // as the original from the target
+        result[key] = targetObject[key];
       }
-    } // go through the source keys
+    }); // go through all the source keys
 
-
-    for (var _i = 0; _i < sourceKeys.length; _i++) {
-      var // get the current key
-      _key = sourceKeys[_i]; // if the target keys doesn't contain the key
-
-      if (targetKeys.indexOf(_key) === -1) {
-        // set the result value to the target value (this will
-        // add new values to the object which are defined in the target)
-        result[_key] = source[_key];
+    sourceKeys.forEach(function (key) {
+      // if the target doesn't contain the key
+      // then the value is new and should be added
+      // to the result object
+      if (targetKeys.indexOf(key) === -1) {
+        result[key] = sourceObject[key];
       }
-    } // return the new object result
-
-
+    });
     return result;
-  }; // go through the targets
+  }
+
+  // internals
+  /**
+   * Merge specified objects into one object with the most right
+   * object having the most priority
+   *
+   * @param {Object} target The base object
+   * @param {...Object} sources The object(s) to merge
+   *
+   * @returns {Object} The merged object(s) result
+   */
+
+  function merge(target) {
+    var targetObject = target;
+
+    if (!isObj(target)) {
+      targetObject = {};
+    } // for all the sources provided merge them with
+    // the target object
 
 
-  for (var i = 0; i < (arguments.length <= 1 ? 0 : arguments.length - 1); i++) {
-    var // get the current target
-    source = i + 1 < 1 || arguments.length <= i + 1 ? undefined : arguments[i + 1];
-
-    if (source !== undefined && source !== null && _typeof(source) === 'object') {
-      // run assign with the source and current target
-      // this will return the deep assigned version of
-      // source with target merged into it
-      // it will then set source to that and if any more
-      // targets are specified it will loop over and repeat
-      // leading to each target overriding the previous
-      // source resulting in the original source overridden
-      // by the array of targets specified
-      target = assign(target, source);
+    for (var _len = arguments.length, sources = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      sources[_key - 1] = arguments[_key];
     }
-  } // return the source (the true result)
 
+    sources.forEach(function (s) {
+      // before merging check the source is an object
+      if (isObj(s)) {
+        targetObject = mergeObject(targetObject, s);
+      }
+    });
+    return targetObject;
+  }
 
-  return target;
-} // export the module
+  return merge;
 
-
-module.exports = circleAssign;
+})));
